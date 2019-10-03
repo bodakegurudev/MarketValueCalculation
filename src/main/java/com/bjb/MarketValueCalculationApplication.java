e161a425fcbc6a5af95ad1bd0c644849b84c3ad5
Bodake, Gurudev Uddhav | RASIA <gurudevuddhav.bodake@rakuten.com>
2019-10-03 20:03:20 +0800

    First Commit

---
+++ src/main/java/com/bjb/MarketValueCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.bjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketValueCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketValueCalculationApplication.class, args);
+	}
+
+}
